ddbaffac96d41502ff0b284e4347f4eba03e330e
Макс Кузнецов <orenburg20172017@gmail.com>
2022-03-17 01:06:06 +0300

    new app

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Исходные значения
+        long price = 10_000;
+        long part = 20;
+
+        // Логика
+        long miles = price / part;
+        System.out.println(miles);
+    }
+}
