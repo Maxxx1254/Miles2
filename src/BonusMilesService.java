886b1303348e4d8427184e394c44beeabf217886
Макс Кузнецов <orenburg20172017@gmail.com>
2022-03-27 00:44:04 +0300

    тест кейс для проверки

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int part = 20;
+        int miles = cost / part;
+        return miles;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // Исходные значения
-        long price = 10_000;
-        long part = 20;
-
-        // Логика
-        long miles = price / part;
-        System.out.println(miles);
+        BonusMilesService bonus = new BonusMilesService();
+        int result =  bonus.calculate( 10_000);
+        System.out.println("Ваш бонус составляет=" + result);
     }
 }
